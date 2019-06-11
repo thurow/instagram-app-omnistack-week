cb3054940bccb62fa21a0d2619f09aace4ec4904
Alexandre Thurow <alexandre.fredericothurow@gmail.com>
2019-06-11 13:36:36 -0300

    instalando dependencia do mongoose e configurando conexao

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,11 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express()
+
+mongoose.connect('mongodb+srv://semana:semana@cluster0-t4az9.mongodb.net/test?retryWrites=true&w=majority', {
+    useNewUrlParser: true
+})
 
 app.get('/', (req, res) => {
     return res.send(`Olá ${req.query.name}`)
