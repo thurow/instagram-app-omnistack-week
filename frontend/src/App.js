bc8919e73616cccf7836eb21515a6bc8edd8b5ed
Alexandre Thurow <alexandre.fredericothurow@gmail.com>
2019-06-12 11:57:06 -0300

    creating routes and page components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import Routes from './routes'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hello World</h1>
-      </div>
+      <BrowserRouter>
+        <Routes />
+      </BrowserRouter>
     );
   }
 }
