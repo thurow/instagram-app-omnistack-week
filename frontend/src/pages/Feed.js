1afa95ba7dafc3f292ffca4fdb8803f2556dcdc6
Alexandre Thurow <alexandre.fredericothurow@gmail.com>
2019-06-12 13:52:26 -0300

    style to feed

--- frontend/src/pages/Feed.js
+++ frontend/src/pages/Feed.js
@@ -1,11 +1,73 @@
 import React, { Component } from 'react'
 
+import './Feed.css'
+
+import more from '../assets/more.svg'
+import like from '../assets/like.svg'
+import comment from '../assets/comment.svg'
+import send from '../assets/send.svg'
+
+
 class Feed extends Component {
     render() {
         return (
-            <h1>
-                Feed
-            </h1>
+            <section id='post-list'>
+                <article>
+                    <header>
+                        <div className='user-info'>
+                            <span>Alexandre Thurow</span>
+                            <span className='place'>Blumenau</span>
+                        </div>
+
+                        <img src={more} alt='Mais' />
+                    </header>
+
+                    <img src='http://localhost:3333/files/Captura de Tela 2019-06-10 às 14.jpg' alt='' />
+
+                    <footer>
+                        <div className='actions'>
+                            <img src={like} alt='' />
+                            <img src={comment} alt='' />
+                            <img src={send} alt='' />
+                        </div>
+
+                        <strong>900 curtidas</strong>
+
+                        <p>
+                            Um post muito massa!
+                            <span>#react #massa</span>
+                        </p>
+                    </footer>
+                </article>
+
+                <article>
+                    <header>
+                        <div className='user-info'>
+                            <span>Alexandre Thurow</span>
+                            <span className='place'>Blumenau</span>
+                        </div>
+
+                        <img src={more} alt='Mais' />
+                    </header>
+
+                    <img src='http://localhost:3333/files/Captura de Tela 2019-06-10 às 14.jpg' alt='' />
+
+                    <footer>
+                        <div className='actions'>
+                            <img src={like} alt='' />
+                            <img src={comment} alt='' />
+                            <img src={send} alt='' />
+                        </div>
+
+                        <strong>900 curtidas</strong>
+
+                        <p>
+                            Um post muito massa!
+                            <span>#react #massa</span>
+                        </p>
+                    </footer>
+                </article>
+            </section>
         )
     }
 }
