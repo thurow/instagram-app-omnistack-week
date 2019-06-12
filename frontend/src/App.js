67f4437833594b4149db966b027e1978a9c6bc7d
Alexandre Thurow <alexandre.fredericothurow@gmail.com>
2019-06-12 13:16:44 -0300

    Created header component and importing assets

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
 import { BrowserRouter } from 'react-router-dom'
+
 import Routes from './routes'
+import Header from './components/Header'
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
+        <Header />
         <Routes />
       </BrowserRouter>
     );
