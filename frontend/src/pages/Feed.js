b1f0a7c9d969e69dc9ef3c1a8de5cd1e0d9ec0bd
Alexandre Thurow <alexandre.fredericothurow@gmail.com>
2019-06-12 18:17:28 -0300

    feed posts coming from api

--- frontend/src/pages/Feed.js
+++ frontend/src/pages/Feed.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import api from '../services/api'
 
 import './Feed.css'
 
@@ -9,64 +10,50 @@ import send from '../assets/send.svg'
 
 
 class Feed extends Component {
+
+    state = {
+        feed: []
+    }
+
+    async componentDidMount() {
+        const response = await api.get('posts')
+
+        this.setState({ feed: response.data })
+    }
+
     render() {
+        const { feed } = this.state
         return (
             <section id='post-list'>
-                <article>
-                    <header>
-                        <div className='user-info'>
-                            <span>Alexandre Thurow</span>
-                            <span className='place'>Blumenau</span>
-                        </div>
+                { feed.map(post => (
+                    <article>
+                        <header>
+                            <div className='user-info'>
+                                <span>{post.author}</span>
+                                <span className='place'>{post.place}</span>
+                            </div>
 
-                        <img src={more} alt='Mais' />
-                    </header>
+                            <img src={more} alt='Mais' />
+                        </header>
 
-                    <img src='http://localhost:3333/files/Captura de Tela 2019-06-10 às 14.jpg' alt='' />
+                        <img src={`http://localhost:3333/files/${post.image}`} alt='' />
 
-                    <footer>
-                        <div className='actions'>
-                            <img src={like} alt='' />
-                            <img src={comment} alt='' />
-                            <img src={send} alt='' />
-                        </div>
+                        <footer>
+                            <div className='actions'>
+                                <img src={like} alt='' />
+                                <img src={comment} alt='' />
+                                <img src={send} alt='' />
+                            </div>
 
-                        <strong>900 curtidas</strong>
+                            <strong>{post.likes} curtidas</strong>
 
-                        <p>
-                            Um post muito massa!
-                            <span>#react #massa</span>
-                        </p>
-                    </footer>
-                </article>
-
-                <article>
-                    <header>
-                        <div className='user-info'>
-                            <span>Alexandre Thurow</span>
-                            <span className='place'>Blumenau</span>
-                        </div>
-
-                        <img src={more} alt='Mais' />
-                    </header>
-
-                    <img src='http://localhost:3333/files/Captura de Tela 2019-06-10 às 14.jpg' alt='' />
-
-                    <footer>
-                        <div className='actions'>
-                            <img src={like} alt='' />
-                            <img src={comment} alt='' />
-                            <img src={send} alt='' />
-                        </div>
-
-                        <strong>900 curtidas</strong>
-
-                        <p>
-                            Um post muito massa!
-                            <span>#react #massa</span>
-                        </p>
-                    </footer>
-                </article>
+                            <p>
+                                {post.description}
+                                <span>{post.hashtags}</span>
+                            </p>
+                        </footer>
+                    </article>
+                )) }
             </section>
         )
     }
