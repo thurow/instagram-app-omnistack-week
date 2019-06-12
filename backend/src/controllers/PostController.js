cde054b5ea6800944878c01599758346ca84ff69
Alexandre Thurow <alexandre.fredericothurow@gmail.com>
2019-06-11 23:24:30 -0300

    changing the application to be compatible for websocket and documenting important lines

--- backend/src/controllers/PostController.js
+++ backend/src/controllers/PostController.js
@@ -4,19 +4,28 @@ const path = require('path')
 const fs = require('fs')
 
 module.exports = {
+
+    /**
+     * Lists all posts sorted by create Date
+     */
     async index(req, res) {
         const posts = await Post.find().sort('-createdAt')
 
         return res.json(posts)
     },
 
+    /**
+     * Create post and save at mongodb
+     */
     async store(req, res) {
         const { author, place, description, hashtags } = req.body
         const { filename: image } = req.file
 
+        // Changing the file extension
         const [name] = image.split('.')
         const fileName = `${name}.jpg`
 
+        // Resizing the photo using sharp
         await sharp(req.file.path)
             .resize(500)
             .jpeg({ quality: 70 })
@@ -24,8 +33,10 @@ module.exports = {
                 path.resolve(req.file.destination, 'resized', fileName)
             )
 
+        // removing original file from path
         fs.unlinkSync(req.file.path)
 
+        // saving post on mongo
         const post = await Post.create({
             author,
             place,
@@ -33,6 +44,9 @@ module.exports = {
             hashtags,
             image: fileName
         })
+
+        // Emitting for all users connected that there is a new post
+        req.io.emit('post', post)
 
         return res.json(post)
     }
--- backend/src/index.js
+++ backend/src/index.js
@@ -5,14 +5,25 @@ const cors = require('cors')
 
 const app = express()
 
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+
 mongoose.connect('mongodb+srv://semana:semana@cluster0-t4az9.mongodb.net/test?retryWrites=true&w=majority', {
     useNewUrlParser: true
 })
 
+// Passing the socket as middleware for all requests
+app.use((req, res, next) => {
+    req.io = io
+
+    next()
+})
+
 app.use(cors())
 
+// static files endpoint to load the images
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')))
 
 app.use(require('./routes'))
 
-app.listen(3333)
+server.listen(3333)
