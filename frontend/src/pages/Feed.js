0fa1ab9b185521142aa9c2c2ea8da8eaec0e3196
Alexandre Thurow <alexandre.fredericothurow@gmail.com>
2019-06-12 22:57:38 -0300

    listening changes with socket.io on frontend

--- frontend/src/pages/Feed.js
+++ frontend/src/pages/Feed.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import api from '../services/api'
+import io from 'socket.io-client'
 
 import './Feed.css'
 
@@ -16,9 +17,27 @@ class Feed extends Component {
     }
 
     async componentDidMount() {
+        this.registerToSocket()
+
         const response = await api.get('posts')
 
         this.setState({ feed: response.data })
+    }
+
+    registerToSocket = () => {
+        const socket = io('http://localhost:3333')
+
+        socket.on('post', newPost => {
+            this.setState({ feed: [newPost, ...this.state.feed] })
+        })
+
+        socket.on('like', likedPost => {
+            this.setState({
+                feed: this.state.feed.map(post =>
+                    post._id === likedPost._id ? likedPost : post
+                )
+            })
+        })
     }
 
     handleLike = id => {
