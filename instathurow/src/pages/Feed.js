bccaa80b5a5fc38eb87eb3ae5304660b7955c1e1
Alexandre Thurow <alexandre.fredericothurow@gmail.com>
2019-06-14 00:15:09 -0300

    handle likes and new posts with socket.io

--- instathurow/src/pages/Feed.js
+++ instathurow/src/pages/Feed.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Text, View, Image, StyleSheet, TouchableOpacity, FlatList } from 'react-native'
 import api from '../services/api'
+import io from 'socket.io-client'
 
 import camera from '../assets/camera.png'
 import more from '../assets/more.png'
@@ -15,11 +16,31 @@ export default class Feed extends Component {
     }
 
     async componentDidMount() {
-        // this.registerToSocket()
+        this.registerToSocket()
 
         const response = await api.get('posts')
 
         this.setState({ feed: response.data })
+    }
+
+    registerToSocket = () => {
+        const socket = io('http://localhost:3333')
+
+        socket.on('post', newPost => {
+            this.setState({ feed: [newPost, ...this.state.feed] })
+        })
+
+        socket.on('like', likedPost => {
+            this.setState({
+                feed: this.state.feed.map(post =>
+                    post._id === likedPost._id ? likedPost : post
+                )
+            })
+        })
+    }
+
+    handleLike = id => {
+        api.post(`/posts/${id}/like`)
     }
 
     static navigationOptions = ({ navigation }) => ({
@@ -51,7 +72,7 @@ export default class Feed extends Component {
 
                             <View style={styles.feedItemFooter}>
                                 <View style={styles.actions}>
-                                    <TouchableOpacity style={styles.action} onPress={() => {}}>
+                                    <TouchableOpacity style={styles.action} onPress={() => this.handleLike(item._id)}>
                                         <Image source={like} />
                                     </TouchableOpacity>
                                     <TouchableOpacity style={styles.action} onPress={() => {}}>
