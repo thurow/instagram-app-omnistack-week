4ce5ed44d9eb5abb90cd7bdb7e93721e7f39364a
Alexandre Thurow <alexandre.fredericothurow@gmail.com>
2019-06-13 23:56:31 -0300

    feed structure

--- instathurow/src/pages/Feed.js
+++ instathurow/src/pages/Feed.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react'
-import { Text, View, Image, TouchableOpacity, FlatList } from 'react-native'
+import { Text, View, Image, StyleSheet, TouchableOpacity, FlatList } from 'react-native'
 import api from '../services/api'
+
 import camera from '../assets/camera.png'
+import more from '../assets/more.png'
+import like from '../assets/like.png'
+import comment from '../assets/comment.png'
+import send from '../assets/send.png'
 
 export default class Feed extends Component {
 
@@ -27,15 +32,48 @@ export default class Feed extends Component {
 
     render() {
         return (
-            <View>
+            <View style={styles.container}>
                 <FlatList
                     data={this.state.feed}
                     keyExtractor={post => post._id}
                     renderItem={({ item }) => (
-                        <Text>{item.author}</Text>
+                        <View style={styles.feedItem}>
+                            <View style={styles.feedItemHeader}>
+                                <View style={styles.userInfo}>
+                                    <Text style={styles.name}>{item.author}</Text>
+                                    <Text style={styles.place}>{item.place}</Text>
+                                </View>
+
+                                <Image source={more} />
+                            </View>
+
+                            <Image style={styles.feedImage} source={{ uri: `http://localhost:3333/files/${item.image}` }} />
+
+                            <View style={styles.feedItemFooter}>
+                                <View style={styles.actions}>
+                                    <TouchableOpacity onPress={() => {}}>
+                                        <Image source={like} />
+                                    </TouchableOpacity>
+                                    <TouchableOpacity onPress={() => {}}>
+                                        <Image source={comment} />
+                                    </TouchableOpacity>
+                                    <TouchableOpacity onPress={() => {}}>
+                                        <Image source={send} />
+                                    </TouchableOpacity>
+                                </View>
+
+                                <Text style={styles.likes}>{item.likes} curtidas</Text>
+                                <Text style={styles.description}>{item.description}</Text>
+                                <Text style={styles.hashtags}>{item.hashtags}</Text>
+                            </View>
+                        </View>
                     )}
                 />
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    
+})
